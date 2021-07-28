b8b07bb1ba45ff9c6169b200d4cd641372eb4331
Joshua <j.blengino@yahoo.com>
2021-07-28 08:54:30 -0700

    question list rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+const questionUrl = "http://localhost:4000/questions"
+
 function App() {
   const [page, setPage] = useState("List");
+  const [ questionsArr, setQuestionsArr] = useState([])
+
+  useEffect(() => {
+    fetch(questionUrl)
+    .then(res => res.json())
+    .then(questions => setQuestionsArr(questions))
+  }, [])
+
+  console.log(questionsArr)
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questionsArr={questionsArr}/>}
     </main>
   );
 }
